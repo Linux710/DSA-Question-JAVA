b1e22ee982d8bb114d7efb7cc9ed85c66a988c36
anmolthakur843 <anmolthakur843@gmail.com>
2022-06-01 22:00:28 +0530

    Update two-sum-problem.java
    Here the two sum problem is solved using Hash Map with O(n). Also the array elements are not sorted.

--- two-sum-problem.java
+++ two-sum-problem.java
@@ -1,4 +1,9 @@
 
+/*O(n) runtime, O(n) space – Hash table:
+We could reduce the runtime complexity of looking up a value to O(1) using a hash map
+that maps a value to its index.*/
+
+
 import java.util.*;
 
 
